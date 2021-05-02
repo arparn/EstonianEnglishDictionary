4e9fc44dd7f461691519b2fa78d5caa3e673f38a
ARTHUR-ALEKSANDER PÄRNOJA <arthurp200148@gmail.com>
2021-05-02 14:03:05 +0300

    getting started

---
+++ src/test/java/Telia/EstonianEnglishDictionary/EstonianEnglishDictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package Telia.EstonianEnglishDictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstonianEnglishDictionaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
